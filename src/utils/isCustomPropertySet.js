1ac74c51f4b3a668db2033fb5996b8fa719b6620
XhmikosR <xhmikosr@gmail.com>
2021-10-26 09:16:01 +0300

    Replace lodash with lodash.get

--- src/utils/isCustomPropertySet.js
+++ src/utils/isCustomPropertySet.js
@@ -1,4 +1,4 @@
-import { get } from "lodash";
+import get from "lodash.get";
 
 /**
  * Check whether a Node is a custom property set
--- src/utils/isStandardRule.js
+++ src/utils/isStandardRule.js
@@ -1,4 +1,4 @@
-import { get } from "lodash";
+import get from "lodash.get";
 import isCustomPropertySet from "./isCustomPropertySet";
 import isStandardSyntaxSelector from "./isStandardSelector";
 
